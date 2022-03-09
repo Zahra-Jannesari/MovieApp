3e8012f0e1633cc2ae55d73f22d216c324ec35bc
Zarisa <jannesari.zarisa@gmail.com>
2022-03-09 13:18:50 +0330

    handle favorite buttons by a very ugly solution

--- app/src/main/java/com/zarisa/netflixclone/HomeFragment.kt
+++ app/src/main/java/com/zarisa/netflixclone/HomeFragment.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.zarisa.netflixclone.databinding.FragmentHomeBinding
-
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
@@ -23,7 +21,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         binding= FragmentHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -33,13 +30,51 @@ class HomeFragment : Fragment() {
         sharedPrefInfo = this.activity?.getSharedPreferences("Information", Context.MODE_PRIVATE)
         (activity as AppCompatActivity).supportActionBar?.title = "NetflixClone"
         registerState=sharedPrefInfo?.getBoolean(isRegistered,false)
-        var isButtonClicked = false
-        binding.radio0.setOnClickListener{
-                isButtonClicked = !isButtonClicked // toggle the boolean flag
-                binding.radio0.setBackgroundResource(if (isButtonClicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        favBtnsSetOnClickListeners()
+    }
 
+
+    private fun favBtnsSetOnClickListeners() {
+        binding.btnFav11.setOnClickListener{
+                StateOfFavButtons.isBtn11Clicked = !StateOfFavButtons.isBtn11Clicked
+                binding.btnFav11.setBackgroundResource(if (StateOfFavButtons.isBtn11Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav12.setOnClickListener{
+            StateOfFavButtons.isBtn12Clicked = !StateOfFavButtons.isBtn12Clicked
+            binding.btnFav12.setBackgroundResource(if (StateOfFavButtons.isBtn12Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav13.setOnClickListener{
+            StateOfFavButtons.isBtn13Clicked = !StateOfFavButtons.isBtn13Clicked
+            binding.btnFav13.setBackgroundResource(if (StateOfFavButtons.isBtn13Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+
+        binding.btnFav21.setOnClickListener{
+            StateOfFavButtons.isBtn21Clicked = !StateOfFavButtons.isBtn21Clicked
+            binding.btnFav21.setBackgroundResource(if (StateOfFavButtons.isBtn21Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav22.setOnClickListener{
+            StateOfFavButtons.isBtn22Clicked = !StateOfFavButtons.isBtn22Clicked
+            binding.btnFav22.setBackgroundResource(if (StateOfFavButtons.isBtn22Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav23.setOnClickListener{
+            StateOfFavButtons.isBtn23Clicked = !StateOfFavButtons.isBtn23Clicked
+            binding.btnFav23.setBackgroundResource(if (StateOfFavButtons.isBtn23Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+
+        binding.btnFav31.setOnClickListener{
+            StateOfFavButtons.isBtn31Clicked = !StateOfFavButtons.isBtn31Clicked
+            binding.btnFav31.setBackgroundResource(if (StateOfFavButtons.isBtn31Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav32.setOnClickListener{
+            StateOfFavButtons.isBtn32Clicked = !StateOfFavButtons.isBtn32Clicked
+            binding.btnFav32.setBackgroundResource(if (StateOfFavButtons.isBtn32Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
+        }
+        binding.btnFav33.setOnClickListener{
+            StateOfFavButtons.isBtn33Clicked = !StateOfFavButtons.isBtn33Clicked
+            binding.btnFav33.setBackgroundResource(if (StateOfFavButtons.isBtn33Clicked) R.drawable.ic_baseline_favorite_selecte_24 else R.drawable.ic_baseline_favorite_notselect_24)
         }
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.option_menu,menu)
@@ -58,16 +93,5 @@ class HomeFragment : Fragment() {
             R.id.btn_comeSoon->findNavController().navigate(R.id.action_homeFragment_to_comingSoonFragment)
         }
         return super.onOptionsItemSelected(item)
-    }
-    fun testFavoriteButton(){
-//        DrawableCompat.setTint(binding.fbtn.drawable, ContextCompat.getColor(requireContext(),R.color.textColor));
-//        val porterDuffColorFilter = PorterDuffColorFilter(
-//            Color.BLUE,
-//            PorterDuff.Mode.SRC_ATOP
-//        )
-//
-//        binding.fbtn.drawable.colorFilter = porterDuffColorFilter
-//        binding.fbtn.drawable.setColorFilter(Color.RED, PorterDuff.Mode.LIGHTEN )
-//binding.imageViewfav.setColorFilter(Color.RED,PorterDuff.Mode.OVERLAY)
     }
 }
---
+++ app/src/main/java/com/zarisa/netflixclone/StateOfFavButtons.kt
@@ -0,0 +1,16 @@
+package com.zarisa.netflixclone
+
+object StateOfFavButtons {
+    var isBtn11Clicked=false
+    var isBtn12Clicked=false
+    var isBtn13Clicked=false
+    var isBtn21Clicked=false
+    var isBtn22Clicked=false
+    var isBtn23Clicked=false
+    var isBtn31Clicked=false
+    var isBtn32Clicked=false
+    var isBtn33Clicked=false
+    var isBtn41Clicked=false
+    var isBtn42Clicked=false
+    var isBtn43Clicked=false
+}
