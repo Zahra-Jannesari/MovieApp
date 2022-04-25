03f111c4896e794b65aedda94d2181def118a7d8
Zarisa <jannesari.zarisa@gmail.com>
2022-04-25 23:04:02 +0430

    add list and method related to liked items

--- app/src/main/java/com/zarisa/netflixclone/adapter/Movie.kt
+++ app/src/main/java/com/zarisa/netflixclone/adapter/Movie.kt
@@ -5,9 +5,18 @@ import com.zarisa.netflixclone.R
 data class Movie(var MovieImage:Int,var movieName:String,var isLiked:Boolean=false)
 object Repository{
     var movieList= mutableListOf<Movie>()
+    var likedMovieList= mutableListOf<Movie>()
     fun addTestDate(){
         for (i in 1..30){
             movieList.add(Movie(R.drawable.acton_example1,"Movie$i"))
+        }
+    }
+    fun fixLikedList(){
+        likedMovieList.clear()
+        for (i in movieList){
+            if (i.isLiked){
+                likedMovieList.add(i)
+            }
         }
     }
 }
