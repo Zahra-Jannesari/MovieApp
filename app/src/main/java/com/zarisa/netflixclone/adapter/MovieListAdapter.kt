d4a38c51dcd305d17f5977a50a2b4a7cad38cb82
Zarisa <jannesari.zarisa@gmail.com>
2022-04-26 00:27:25 +0430

    refactor adaptor by recycler adapter

--- app/src/main/java/com/zarisa/netflixclone/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/zarisa/netflixclone/adapter/MovieListAdapter.kt
@@ -13,48 +13,38 @@ import com.zarisa.netflixclone.databinding.MovieListItemBinding
 
 typealias like=((button: ImageButton, movie:Movie)->Unit)
 
-class MovieListAdapter (var context: Context, var movieList: MutableList<Movie> = mutableListOf()):BaseAdapter(){
-
-//    inner class Holder(view: View) : RecyclerView.ViewHolder(view) {
-//        val itemImage=view.findViewById<ImageView>(R.id.imageView_movieImage)
-//        val itemName=view.findViewById<TextView>(R.id.textView_movieName)
-//        val itemBtnFav=view.findViewById<ImageButton>(R.id.btn_movieFav)
-//
-//        fun bind(movie: Movie){
-//            itemImage.setImageResource(movie.MovieImage)
-//            itemName.text=movie.movieName
-//            itemBtnFav.isSelected=movie.isLiked
-//        }
-//    }
-    override fun getCount(): Int {
-        return movieList.size
+class MovieListAdapter ( val onItemClick:like):
+    ListAdapter<Movie, MovieListAdapter.MovieHolder>(MovieDiffCallback) {
+    inner class MovieHolder(val binding: MovieListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Movie) {
+            try {
+                binding.imageViewMovieImage.setImageResource(item.MovieImage)
+                binding.textViewMovieName.text=item.movieName
+                binding.btnMovieFav.isSelected=item.isLiked
+                binding.btnMovieFav.setOnClickListener { onItemClick(binding.btnMovieFav,item) }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
-    override fun getItem(p0: Int): Any {
-        return movieList[p0]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListAdapter.MovieHolder {
+        val binding= MovieListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MovieHolder(binding)
     }
 
-    override fun getItemId(p0: Int): Long {
-        return p0.toLong()
-    }
-
-    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        var view:View= View.inflate(context, R.layout.movie_list_item,null)
-
-        val itemImage=view.findViewById<ImageView>(R.id.imageView_movieImage)
-        val itemName=view.findViewById<TextView>(R.id.textView_movieName)
-        val itemBtnFav=view.findViewById<ImageButton>(R.id.btn_movieFav)
-
-        var movie= movieList[p0]
-
-        itemImage.setImageResource(movie.MovieImage)
-        itemName.text=movie.movieName
-        itemBtnFav.isSelected=movie.isLiked
-        return view
-    }
-
-    fun addList(mutableList: MutableList<Movie>){
-        movieList=mutableList
+    override fun onBindViewHolder(holder: MovieListAdapter.MovieHolder, position: Int) {
+        val movie = getItem(position)
+        holder.bind(movie)
     }
 }
+object MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+    }
 
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
+    }
+}
