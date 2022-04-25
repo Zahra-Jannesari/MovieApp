0d09ee3037f30bcac3efa45a3aa2a6eba5a26109
Zarisa <jannesari.zarisa@gmail.com>
2022-04-25 21:34:47 +0430

    add dataTest

--- app/src/main/java/com/zarisa/netflixclone/adapter/Movie.kt
+++ app/src/main/java/com/zarisa/netflixclone/adapter/Movie.kt
@@ -1,3 +1,13 @@
 package com.zarisa.netflixclone.adapter
 
-data class Movie(var MovieImage:Int,var movieName:String,var isLiked:Boolean)
+import com.zarisa.netflixclone.R
+
+data class Movie(var MovieImage:Int,var movieName:String,var isLiked:Boolean=false)
+object Repository{
+    var movieList= mutableListOf<Movie>()
+    fun addTestDate(){
+        for (i in 1..30){
+            movieList.add(Movie(R.drawable.acton_example1,"Movie$i"))
+        }
+    }
+}
